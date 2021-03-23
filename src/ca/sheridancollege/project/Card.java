1bfa3eee91885a448b23b9b4382d9443fe3f9554
Rhainel <rhainelperalta@gmail.com>
2021-03-23 11:59:17 -0400

    modified card and group of cards classes

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -2,6 +2,8 @@
  * SYST 17796 Project Winter 2019 Base code.
  * Students can modify and extend to implement their game.
  * Add your name as a modifier and the date!
+ * 
+ * modified by: Rhainel 23-03-21
  */
 package ca.sheridancollege.project;
 
@@ -11,17 +13,38 @@ package ca.sheridancollege.project;
  * should remember to add themselves as a modifier.
  * @author dancye, 2018
  */
-public abstract class Card 
+public class Card 
 {
-    //default modifier for child classes
+    final private int value;
+    final private int suit;
     
-    /**
-     * Students should implement this method for their specific children classes 
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
+    
+    final private String[] values = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
+    
+    final private String[]  suits = {"Spades", "Clubs", "Hearts", "Diamonds"};
+    
+    
+    public Card(int value, int suit)
+    {
+        this.value = value;
+        this.suit = suit;
+    }
+    
+    public int getValue()
+    {
+        return value;
+    }
+    
+    public int getSuit()
+    {
+        return suit;
+    }
     
     @Override
-    public abstract String toString();
+    public String toString()
+    {
+        return values[value] + "of " + suits[suit];
+    }
     
 }
 
--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -17,40 +17,17 @@ import java.util.Collections;
 public class GroupOfCards 
 {
     //The group of cards, stored in an ArrayList
-    private ArrayList <Card> cards;
+    private ArrayList <Card> deck;
     private int size;//the size of the grouping
     
-    public GroupOfCards(int givenSize)
-    {
-        size = givenSize;
-    }
-    
-    /**
-     * A method that will get the group of cards as an ArrayList
-     * @return the group of cards.
-     */
     public ArrayList<Card> showCards()
     {
-        return cards;
+        return deck;
     }
     
     public void shuffle()
     {
-        Collections.shuffle(cards);
-    }
-
-    /**
-     * @return the size of the group of cards
-     */
-    public int getSize() {
-        return size;
-    }
-
-    /**
-     * @param givenSize the max size for the group of cards
-     */
-    public void setSize(int givenSize) {
-        size = givenSize;
+        Collections.shuffle(deck);
     }
     
 }//end class
